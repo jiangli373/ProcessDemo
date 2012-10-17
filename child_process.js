7a9677ea2d579a1bfda905d8ca086586a89a6b6b
jiangli <liminghaoze@qq.com>
2012-10-17 11:46:49 +0800

    child_process使用exec

--- child_process.js
+++ child_process.js
@@ -5,10 +5,12 @@
  * Time: 下午3:19
  * To change this template use File | Settings | File Templates.
  */
-var spawn = require('child_process').spawn;
 
-//使用 spawn
-var  ls  = spawn('ls', ['-lh', '/usr']);//command是命令,[]中是执行命令的参数
+var exec = require('child_process').exec
+
+
+//使用exec
+var ls = exec('ls -l /usr'); //command是一个完整的命令
 
 ls.stdout.on('data', function (data) {
     console.log('stdout: ' + data);
@@ -16,9 +18,9 @@ ls.stdout.on('data', function (data) {
 
 ls.stderr.on('data', function (data) {
     console.log('stderr: ' + data);
+
 });
 
-ls.on('exit',function(code){
+ls.on('exit', function (code) {
     console.log('child process exited with code ' + code);
 });
-
