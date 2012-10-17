f92621b29b9cdb5b5eb670978ae5ce2b3eacf67f
jiangli <liminghaoze@qq.com>
2012-10-17 11:11:17 +0800

    修改命名

--- app.js
+++ app.js
@@ -3,28 +3,12 @@
  */
 var express = require('express')
     , routes = require('./routes');
-
 var app = express();
 
 //使用child_process
 var spawn = require('child_process').spawn;
 
-//使用node-webshot
-var webshot = require('webshot');
-var options = {
-//    screenSize: {
-//        width: 320
-//        , height: 280
-//    }
-//    shotSize:{
-//        width:'window',
-//        height:'window'
-//    }
-//    , userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us)'
-//        + ' AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g'
-}
 // Configuration
-
 app.configure(function () {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
@@ -47,25 +31,18 @@ app.get('/', routes.index);
 app.post('/cutcover', function (req, res) {
     var imageName = __dirname + '/public/images/img.jpg';
 
-     //使用child_process
-     var phantomjs = spawn('phantomjs', [__dirname +'/rasterize.js', req.body.weburl, imageName]);
-     phantomjs.stdout.on('data', function (data) {
-     console.log('stdout:' + data);
-     });
-     phantomjs.stderr.on('data', function (data) {
-     console.log('stderr:' + data);
-     });
-     phantomjs.on('exit', function (code) {
-     console.log('exit:' + code);
-     res.send({'imgurl':imageName});
-     });
-
-
-    //使用node-webshot
-//    webshot(req.body.weburl, imageName, options, function (err) {
-//        if (err) return console.log(err);
-//        res.send({'imgurl':imageName});
-//    });
+    //使用child_process
+    var phantomjs = spawn('phantomjs', [__dirname + '/rasterize.js', req.body.weburl, imageName]);
+    phantomjs.stdout.on('data', function (data) {
+        console.log('stdout:' + data);
+    });
+    phantomjs.stderr.on('data', function (data) {
+        console.log('stderr:' + data);
+    });
+    phantomjs.on('exit', function (code) {
+        console.log('exit:' + code);
+        res.send({'imgurl':imageName});
+    });
 
 });
 
